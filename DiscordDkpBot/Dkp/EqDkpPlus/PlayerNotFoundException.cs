bbbc6c2149e4cf2e9c41a5c1294287e7a6d26de2
Josh Sterling <Josh@Sterling.digital>
2019-04-09 21:18:18 -0700

    Fixed player not found exception not reporting properly.

--- DiscordDkpBot/Dkp/EqDkpPlus/PlayerNotFoundException.cs
+++ DiscordDkpBot/Dkp/EqDkpPlus/PlayerNotFoundException.cs
@@ -2,7 +2,7 @@
 
 namespace DiscordDkpBot.Dkp.EqDkpPlus
 {
-	public class PlayerNotFoundException : Exception
+	public class PlayerNotFoundException : DkpBotException
 	{
 		public PlayerNotFoundException (string player) : base($"Could not find player '{player}'.")
 		{
