2988bbdc2c760bccc90b6e11e4c6cc2955581083
Josh Sterling <Josh@Sterling.digital>
2019-05-28 22:24:42 -0700

    Fixed Cancel auction command

--- DiscordDkpBot/Commands/CancelAuctionCommand.cs
+++ DiscordDkpBot/Commands/CancelAuctionCommand.cs
@@ -14,37 +14,58 @@ namespace DiscordDkpBot.Commands
 {
 	public class CancelAuctionCommand : IChannelCommand
 	{
-		private static readonly string[] CommandTriggers = { "cancelbids", "cancel", "cancelbid"};
-		private readonly DkpBotConfiguration configuration;
+		private static readonly string[] CommandTriggers = { "cancelbids", "cancel", "cancelbid" };
 		private readonly IAuctionProcessor auctionProcessor;
+		private readonly DkpBotConfiguration configuration;
 		private readonly ILogger<CancelAuctionCommand> log;
 		private readonly Regex pattern;
 		public string ChannelSyntax => $"{configuration.CommandPrefix} {CommandTriggers.First()} \"ItemName\"";
 
 		public CancelAuctionCommand(DkpBotConfiguration configuration, IAuctionProcessor auctionProcessor, ILogger<CancelAuctionCommand> log)
 		{
-			string regex = "^" + Regex.Escape(configuration?.CommandPrefix) + "?(?<trigger>" + string.Join('|', CommandTriggers) + @")?\s*""(?<name>.+)""\s*$";
+			string regex = "^" + Regex.Escape(configuration?.CommandPrefix) + "\\s*(?<trigger>" + string.Join('|', CommandTriggers) + @")?\s*""(?<name>.+)""\s*$";
 			pattern = new Regex(regex, RegexOptions.IgnoreCase);
 			this.configuration = configuration;
 			this.auctionProcessor = auctionProcessor;
 			this.log = log;
 		}
 
-		public async Task<bool> TryInvokeAsync(IMessage message)
+		public (bool success, string name) ParseArgs(string messageContent)
 		{
-			Match match = pattern.Match(message.Content);
+			Match match = pattern.Match(messageContent);
 
 			if (!match.Success)
 			{
 				log.LogTrace("Did not match pattern.");
-				return false;
+				return (false, null);
 			}
 
 			string name = match.Groups["name"].Value;
 			log.LogDebug("Parsed cancel arguments: {0}", name);
+			return (true,name);
+		}
 
-			await auctionProcessor.CancelAuction(name, message);
-			return true;
+		public async Task<bool> TryInvokeAsync(IMessage message)
+		{
+			if (message == null)
+			{
+				return false;
+			}
+			(bool success, string name) = ParseArgs(message.Content);
+
+			if (!success)
+			{
+				return false;
+			}
+			else if (message.Channel is IPrivateChannel)
+			{
+				return false;
+			}
+			else
+			{
+				await auctionProcessor.CancelAuction(name, message);
+				return true;
+			}
 		}
 	}
 }
---
+++ DiscordDkpBotTests/Commands/CancelAuctionCommandTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using DiscordDkpBot.Auctions;
+using DiscordDkpBot.Commands;
+using DiscordDkpBot.Configuration;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace DiscordDkpBotTests.Commands
+{
+	[TestFixture]
+	public class CancelAuctionCommandTests
+	{
+		[TestCase("@ Cancelbids \"YoMomma\"", "YoMomma")]
+		[TestCase("@ cancelbids \"YoMomma\"", "YoMomma")]
+		[TestCase("@ Cancel \"YoMomma\"", "YoMomma")]
+		[TestCase("@ cancel \"YoMomma\"", "YoMomma")]
+		[TestCase("@ Cancelbid \"YoMomma\"", "YoMomma")]
+		[TestCase("@ cancelbid \"YoMomma\"", "YoMomma")]
+		[TestCase("@ cAnCel \"YoMomma\"", "YoMomma")]
+		[TestCase("@ cancel \"Momma\"", "Momma")]
+		[TestCase("@ cancel \"Foo\"", "Foo")]
+		[TestCase("@ cancel \"Some Item With Spaces\"", "Some Item With Spaces")]
+		[TestCase("@ cancel \"Hyphenated-Loots\"", "Hyphenated-Loots")]
+		public void ParseArgs(string args, string expectedName)
+		{
+			//Arrange
+			config.CommandPrefix = "@";
+			target = new CancelAuctionCommand(config, processor.Object, log.Object);
+
+			//Act
+			(bool success,  string name) = target.ParseArgs(args);
+
+			//Assert
+			Assert.AreEqual(expectedName, name);
+		}
+
+		#region Setup/Teardown
+
+		[SetUp]
+		public void SetUp()
+		{
+			config = new DkpBotConfiguration();
+			processor = new Mock<IAuctionProcessor>();
+			log = new Mock<ILogger<CancelAuctionCommand>>();
+		}
+
+		#endregion
+
+		#region Test Helpers
+
+		private DkpBotConfiguration config;
+
+		private Mock<ILogger<CancelAuctionCommand>> log;
+		private Mock<IAuctionProcessor> processor;
+		private CancelAuctionCommand target;
+
+		#endregion
+	}
+}
