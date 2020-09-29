338026465aa29fed3fd6c5e03ee01944c518d86d
Josh Sterling <Josh@Sterling.digital>
2020-09-28 19:39:41 -0700

    #14 - Added StartMultipleAuctionsCommand

---
+++ DiscordDkpBot/Commands/StartMultipleAuctionsCommand.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Discord;
+
+using DiscordDkpBot.Auctions;
+using DiscordDkpBot.Configuration;
+
+using Microsoft.Extensions.Logging;
+
+namespace DiscordDkpBot.Commands
+{
+	public class StartMultipleAuctionsCommand : IChannelCommand
+	{
+		private static readonly string[] CommandTriggers = { "startmany", "startmultiple" };
+		private readonly IAuctionProcessor auctionProcessor;
+		private readonly DiscordConfiguration configuration;
+		private readonly ILogger<StartMultipleAuctionsCommand> log;
+		private readonly Regex pattern;
+		private readonly Regex itemPattern;
+
+		public string ChannelSyntax => $"{configuration.CommandPrefix} {CommandTriggers.First()}:\nItem1\n2xItem2\nEpicness, The Most Epic of Epics\netc.\netc.";
+		public string CommandDescription => "Start Multiple Item Auctions Using Default Duration (No quotes needed. One item per line)";
+
+		public StartMultipleAuctionsCommand(DiscordConfiguration configuration, IAuctionProcessor auctionProcessor, ILogger<StartMultipleAuctionsCommand> log)
+		{
+			string regex = "^" + Regex.Escape(configuration.CommandPrefix) + @"\s*(?<trigger>" + string.Join('|', CommandTriggers) + @"):?\s*\n?(?<items>(?:\s*\d*x?.+)+)";
+			pattern = new Regex(regex, RegexOptions.IgnoreCase);
+
+			itemPattern = new Regex("(?:^\\s*(?<quantity>\\d+)*x?\\s*(?<name>.+?)\\s*$)+", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+			this.configuration = configuration;
+			this.auctionProcessor = auctionProcessor;
+			this.log = log;
+		}
+
+		public async Task<bool> TryInvokeAsync(IMessage message)
+		{
+			if (message == null)
+			{
+				return false;
+			}
+			else if (message.Channel is IPrivateChannel)
+			{
+				return false;
+			}
+			else if (message.Channel.Name != configuration.SilentAuctionsChannelName)
+			{
+				return false;
+			}
+
+			(bool success, Dictionary<string, int> items) = ParseArgs(message.Content);
+
+			if (!success)
+			{
+				return false;
+			}
+			else
+			{
+				List<Task> auctions = new List<Task>();
+				foreach (KeyValuePair<string, int> item in items)
+				{
+					auctions.Add(auctionProcessor.StartAuction(item.Value, item.Key, null, message));
+				}
+				await Task.WhenAll(auctions);
+
+				return true;
+			}
+
+		}
+
+		public (bool success, Dictionary<string, int> items) ParseArgs(string args)
+		{
+			args = args.Replace('“', '"').Replace('”', '"');
+			Match match = pattern.Match(args);
+
+			if (!match.Success || !match.Groups["items"].Success)
+			{
+				return (false, new Dictionary<string, int>());
+			}
+
+			string itemsString = match.Groups["items"].Value;
+			IEnumerable<Match> itemsMatch = itemPattern.Matches(itemsString);
+
+			Dictionary<string, int> items = new Dictionary<string, int>();
+			foreach (Match itemMatch in itemsMatch)
+			{
+				string name = itemMatch.Groups["name"].Value;
+				if (! int.TryParse(itemMatch.Groups["quantity"]?.Value, out int quantity))
+				{
+					quantity = 1;
+				}
+
+				if (!items.ContainsKey(name))
+				{
+					items.Add(name, 0);
+				}
+
+				items[name]+= quantity;
+			}
+
+			return (true, items);
+		}
+	}
+}
---
+++ DiscordDkpBotTests/Commands/StartMultipleAuctionsCommandTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Discord;
+
+using DiscordDkpBot.Auctions;
+using DiscordDkpBot.Commands;
+using DiscordDkpBot.Configuration;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using Match = System.Text.RegularExpressions.Match;
+
+namespace DiscordDkpBotTests.Commands
+{
+	[TestFixture]
+	public class StartMultipleAuctionCommandTests
+	{
+		[TestCase("@startmany:\nFoo1\nFoo2", true, "1xFoo1|1xFoo2")]
+		[TestCase("@startmultiple:\nFoo1\nFoo2", true, "1xFoo1|1xFoo2")]
+		[TestCase("@startmany:\nFoo1\n3xFoo2", true, "1xFoo1|3xFoo2")]
+		[TestCase("@startmany:\nFoo1\n3x Foo2", true, "1xFoo1|3xFoo2")]
+		[TestCase("@startmany:\nFoo, the Barer of Baz\nFoo2", true, "1xFoo, the Barer of Baz|1xFoo2")]
+		[TestCase("@startmany:Foo1\nFoo2", true, "1xFoo1|1xFoo2")]
+		[TestCase("@startmany:\n  3xFoo1\nFoo2", true, "3xFoo1|1xFoo2")]
+		[TestCase("@startmany:\n  Foo1\n  3x Foo2", true, "1xFoo1|3xFoo2")]
+		[TestCase("@startmany:\nFoo1\t\t\n\t3x\tFoo2", true, "1xFoo1|3xFoo2")]
+		[TestCase("@startmany:\nQenni's Mom\n2x Caub's Balls\nLivin's Life", true, "1xQenni's Mom|2xCaub's Balls|1xLivin's Life")]
+		[TestCase("@kjhaegr asdawef", false, "")]
+		public void ParseArgs(string args, bool expectedSuccess, string expectedString)
+		{
+			//Arrange
+			Dictionary<string, int> expectedItems = ParseExpected(expectedString);
+
+			//Act
+			(bool success, Dictionary<string, int> items) = target.ParseArgs(args);
+
+			//Assert
+			success.Should().Be(expectedSuccess);
+			items.Should().BeEquivalentTo(expectedItems);
+		}
+
+		[Test]
+		public async Task StartsMultipleAuctions()
+		{
+			//Arrange
+			string input = "@startmany:Foo1\nFoo2\n2xFoo3";
+			Mock<IMessage> message = new Mock<IMessage>();
+			Mock<IMessageChannel> channel = new Mock<IMessageChannel>();
+			channel.SetupGet(x => x.Name).Returns(config.SilentAuctionsChannelName);
+			message.SetupGet(x => x.Channel).Returns(channel.Object);
+			message.SetupGet(x => x.Content).Returns(input);
+
+			//Act
+			await target.TryInvokeAsync(message.Object);
+
+			//Assert
+			processor.Verify(x => x.StartAuction(1, "Foo1", null, message.Object), Times.Once);
+			processor.Verify(x => x.StartAuction(1, "Foo2", null, message.Object), Times.Once);
+			processor.Verify(x => x.StartAuction(2, "Foo3", null, message.Object), Times.Once);
+		}
+
+		#region Setup/Teardown
+
+		[SetUp]
+		public void SetUp()
+		{
+			config = new DiscordConfiguration();
+			config.CommandPrefix = "@";
+			processor = new Mock<IAuctionProcessor>();
+			log = new Mock<ILogger<StartMultipleAuctionsCommand>>();
+			target = new StartMultipleAuctionsCommand(config, processor.Object, log.Object);
+		}
+
+		#endregion
+
+		#region Test Helpers
+
+		private DiscordConfiguration config;
+		private static readonly Regex expectedItemFormat = new Regex(@"(?<quantity>\d+)x(?<name>.*)");
+
+		private Mock<ILogger<StartMultipleAuctionsCommand>> log;
+		private Mock<IAuctionProcessor> processor;
+		private StartMultipleAuctionsCommand target;
+
+		private Dictionary<string, int> ParseExpected(string expected)
+		{
+			Dictionary<string, int> expectedItems = new Dictionary<string, int>();
+			string[] itemStrings = expected.Split(new[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+
+			foreach (string itemString in itemStrings)
+			{
+				Match match = expectedItemFormat.Match(itemString);
+
+				if (match.Success)
+				{
+					expectedItems.Add(match.Groups["name"].Value, int.Parse(match.Groups["quantity"].Value));
+				}
+				else
+				{
+					throw new InvalidOperationException("Could not parse expected items!");
+				}
+			}
+
+			return expectedItems;
+		}
+
+		#endregion
+	}
+}
